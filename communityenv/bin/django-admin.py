64d90fcd943ebde36ae454b3d56c07585a7d1c30
Patrick Harding <patrickharding@patrickharding.local>
2016-05-24 12:28:37 -0600

    initial commit

---
+++ communityenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/creative/domains/slenderiiz-community.creative.us.ariix.com/community/communityenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
